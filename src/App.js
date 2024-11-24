fdb818b6fcafcfb6be78c0b9dcf9c16d2099899a
nobuyuki <nobuyuki.galois@gmail.com>
2024-11-24 15:01:32 +0900

    learn prop-types

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import PropTypes from 'prop-types';
 
 function App() {
   const profiles = [
@@ -8,6 +9,7 @@ function App() {
     {name: "Jun", age:12},
     {name: "ichiro", age:18},
     {name: "No name",},
+    {name: 1, age: "aaa"},
   ]
   return (
     <div className="App">
@@ -19,7 +21,7 @@ function App() {
             return <User name={profiles.name} age={profiles.age} key={index} />
           })
         }
-        {/* <User name={"Taro"} age={10}/> */}
+        <User name={1} age={"Taro"}/>
       </header>
     </div>
   );
@@ -29,8 +31,13 @@ const User = (props) => {
   return <div>Hi, I am {props.name} and {props.age} years old!</div>
 }
 
-User.defaultProps = {
-  age: 1
+// User.defaultProps = {
+//   age: 1
+// }
+
+User.propTypes = {
+  name: PropTypes.string,
+  age: PropTypes.number.isRequired,
 }
 
 export default App;
